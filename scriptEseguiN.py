9f07e163beec38f8a8d933e501398cc537ab4fe7
pablo <pablo.martellucci@mail.polimi.it>
2024-09-27 13:20:33 +0200

    modifica minore

--- scriptEseguiN.py
+++ scriptEseguiN.py
@@ -1,13 +1,15 @@
 import subprocess
+import datetime
 
 
 def main(N, script):
     for i in range(N):
-        print(f"Esecuzione {i + 1} di {N}")
+        current_time = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+        print(f"{current_time}: Esecuzione {i + 1} di {N}")
         subprocess.run(script, shell=True)
 
 
 if __name__ == '__main__':
-    N = 20                                  # numero di esecuzioni
+    N = 10                                  # numero di esecuzioni
     script = 'python muoviBandieraV3.py'    # script da eseguire N volte
     main(N, script)
